adbe335f7ce54488691a5c1335d9be7bc5419874
Igorein <kargalisamet@gmail.com>
2023-03-17 11:17:02 +0300

    One and Only commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// ISIM
+let userName = prompt("Enter your name!");
+let greeting = document.querySelector(".greeting");
+greeting.innerHTML = `
+Merhaba, <strong>${userName}</strong>! Hosgeldin!
+`
+
+
+// TARIH ZAMAN FUNCTION
+function tarihSaat() {
+    let options = { weekday: 'long' }
+    let date = new Date().toLocaleDateString("tr-TR", options);
+    let time = new Date().toLocaleTimeString();
+    
+    // TARIH
+    let tarih = document.querySelector(".tarih");
+    tarih.innerHTML = `
+Bugün günlerden ${date}
+`
+
+    // ZAMAN
+    let saat = document.querySelector(".saat");
+    saat.innerHTML = `
+Saat: ${time} ve 
+`
+}
+
+setInterval(tarihSaat, 1000);
